42682debc1036d39a5d4ca7fdc5da31030e38902
Bladekmm <148947608+Bladekmm@users.noreply.github.com>
2023-12-02 19:04:16 +0800

    Update CommentForm.tsx

--- component/CommentForm.tsx
+++ component/CommentForm.tsx
@@ -3,23 +3,19 @@
 import React, { useState } from 'react';
 
 interface CommentFormProps {
-    onSubmit: (orderId: number, comment: string) => void;
+    onSubmit: (comment: string) => void;
 }
 
 const CommentForm: React.FC<CommentFormProps> = ({ onSubmit }) => {
-    const [orderId, setOrderId] = useState<number>(0);
     const [comment, setComment] = useState<string>('');
 
     const handlePostComment = () => {
-        onSubmit(orderId, comment);
-        setOrderId(0);
+        onSubmit(comment);
         setComment('');
     };
 
     return (
         <div>
-            <label htmlFor="orderId">Order ID:</label>
-            <input type="text" id="orderId" value={orderId} onChange={(e) => setOrderId(Number(e.target.value))} />
             <label htmlFor="comment">Comment:</label>
             <input type="text" id="comment" value={comment} onChange={(e) => setComment(e.target.value)} />
             <button onClick={handlePostComment}>Post Comment</button>
@@ -28,3 +24,4 @@ const CommentForm: React.FC<CommentFormProps> = ({ onSubmit }) => {
 };
 
 export default CommentForm;
+
